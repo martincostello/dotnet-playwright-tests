aabe46afc7b326c25d2a8ae01a661150606e9bc5
martincostello <martin@martincostello.com>
2021-05-29 12:39:28 +0100

    Move test cases to class data
    Use a [ClassData] attribute to manage the test cases for different browsers.
    Remove Xunit.SkippableFact.

---
+++ PlaywrightTests/BrowsersTestData.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PlaywrightTests
+{
+    public sealed class BrowsersTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "chromium" };
+            yield return new object[] { "firefox" };
+
+            if (OperatingSystem.IsMacOS())
+            {
+                yield return new object[] { "webkit" };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
