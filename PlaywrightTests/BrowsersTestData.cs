2dcaaa2f481f64612f9397f4e719c83f02a7e5d1
martincostello <martin@martincostello.com>
2021-12-14 11:52:07 +0000

    Enable implicit using statements
    Enable the use of C# 10 implicit using statements.

--- PlaywrightTests/BrowsersTestData.cs
+++ PlaywrightTests/BrowsersTestData.cs
@@ -1,9 +1,7 @@
 // Copyright (c) Martin Costello, 2021. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using Microsoft.Playwright;
 
 namespace PlaywrightTests
--- PlaywrightTests/SearchTests.cs
+++ PlaywrightTests/SearchTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Martin Costello, 2021. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Threading.Tasks;
 using Microsoft.Playwright;
 using Xunit;
 using Xunit.Abstractions;
