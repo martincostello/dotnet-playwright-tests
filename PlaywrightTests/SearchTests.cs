a53a0e5194033d8ade2c862aeec19196bbbc7ebf
martincostello <martin@martincostello.com>
2022-01-21 13:23:28 +0000

    Add test timeout
    Add a timeout to the tests to try and help diagnose the Linux and macOS issue.

--- PlaywrightTests/SearchTests.cs
+++ PlaywrightTests/SearchTests.cs
@@ -16,7 +16,7 @@ public class SearchTests
 
     private ITestOutputHelper OutputHelper { get; }
 
-    [Theory]
+    [Theory(Timeout = 30_000)]
     [ClassData(typeof(BrowsersTestData))]
     public async Task Search_For_DotNet_Core(string browserType, string browserChannel)
     {
