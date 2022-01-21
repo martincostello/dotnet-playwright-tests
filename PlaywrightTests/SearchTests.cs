a7db71735f4366080c3f41d09ca435e51a9eb5a7
martincostello <martin@martincostello.com>
2022-01-21 18:46:30 +0000

    Remove the timeout
    Now the hang has been resolved, just let it take as long as it takes.

--- PlaywrightTests/SearchTests.cs
+++ PlaywrightTests/SearchTests.cs
@@ -16,7 +16,7 @@ public class SearchTests
 
     private ITestOutputHelper OutputHelper { get; }
 
-    [Theory(Timeout = 60_000)]
+    [Theory]
     [ClassData(typeof(BrowsersTestData))]
     public async Task Search_For_DotNet_Core(string browserType, string browserChannel)
     {
