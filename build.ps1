3dc5e330d20cef17cb575224df324061a5e0fe2a
Martin Costello <martin@martincostello.com>
2023-11-30 18:26:53 +0000

    Use MSBUILDTERMINALLOGGER
    Use the `MSBUILDTERMINALLOGGER` environment variable instead of specifying `--tl`.

--- build.ps1
+++ build.ps1
@@ -1,5 +1,9 @@
 #! /usr/bin/env pwsh
 
+if ($null -eq $env:MSBUILDTERMINALLOGGER) {
+    $env:MSBUILDTERMINALLOGGER = "auto"
+}
+
 $additionalArgs = @()
 
 if (![string]::IsNullOrEmpty($env:GITHUB_SHA)) {
@@ -7,4 +11,4 @@ if (![string]::IsNullOrEmpty($env:GITHUB_SHA)) {
     $additionalArgs += "GitHubActions;report-warnings=false"
 }
 
-dotnet test ./PlaywrightTests --configuration Release --output ./artifacts --tl $additionalArgs
+dotnet test ./PlaywrightTests --configuration Release --output ./artifacts $additionalArgs
